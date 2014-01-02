600ccf87042d66f34cabe3a5051132f337225a10
phani.08422 <phani08422@gmail.com>
2014-01-02 12:33:55 +0530

    log message

--- RssFeeds/MTTestViewController.m
+++ RssFeeds/MTTestViewController.m
@@ -26,6 +26,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSLog(@"View did load");
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
